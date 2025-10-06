5377e21faf4e5222bf56e3c5e02b961b9b773ca6
persianProgrammers <abdollamemar@gmail.com>
2025-10-06 10:34:18 +0330

    fix AB: fixing some bugs in Home Page

--- src/api/foods.js
+++ src/api/foods.js
@@ -7,12 +7,3 @@ export const getPopularFoodByRandomCategory = () =>
 
 export const getPopularFoodByRandomCategoryExcluding = (excludeTitles = []) =>
     publicAxios.post("/food/popular-foods-excluding", excludeTitles).then(r => r.data);
-
-// Random popular foods
-// export const getRandomPopularFoods = (count = 8) =>
-//     publicAxios.get(`/food/popular-foods?count=${count}`).then(r => r.data);
-
-// // Random excluding some categories
-// export const getRandomPopularFoodsExcluding = (usedCategories) =>
-//     publicAxios.post(`/food/popular-foods-excluding`, usedCategories).then(r => r.data);
-

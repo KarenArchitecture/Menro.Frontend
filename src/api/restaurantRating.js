594bc5093b8de157559cbbd4ed9b2f14c5b55028
persianProgrammers <abdollamemar@gmail.com>
2026-08-23 10:51:13 +0330

    AB feat: restaurant rating stablished

---
+++ src/api/restaurantRating.js
@@ -0,0 +1,7 @@
+import userAxios from "./userAxios";
+
+export const submitRestaurantRating = (restaurantId, score) =>
+  userAxios.post("/restaurant-rating", { restaurantId, score }).then((r) => r.data);
+
+export const getMyRestaurantRating = (restaurantId) =>
+  userAxios.get(`/restaurant-rating/${restaurantId}`).then((r) => r.data);
